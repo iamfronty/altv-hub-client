06c43d771155cfebed74559e547bed6586bff0ea
Stuyk <stuykgaming@gmail.com>
2020-07-09 18:55:39 -0600

    Fix Resource Binding

---
+++ precache-manifest.d72544dd1ef3489f1f89dadd4023ac07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c6260f8a311942c7b8",
+    "url": "css/app.32cd50d1.css"
+  },
+  {
+    "revision": "1268d62960ba5a2d5209dbbdc3834cc5",
+    "url": "img/background.1268d629.png"
+  },
+  {
+    "revision": "e40030420bc15132f3d45d9be6370b86",
+    "url": "img/logo.e4003042.svg"
+  },
+  {
+    "revision": "c881ccfb65170f81cefb5f0652e43c69",
+    "url": "index.html"
+  },
+  {
+    "revision": "76c6260f8a311942c7b8",
+    "url": "js/app.8c4c5603.js"
+  },
+  {
+    "revision": "036fbb8ecea87567d9aa",
+    "url": "js/chunk-vendors.110a1bc2.js"
+  },
+  {
+    "revision": "664ce1e7799651861e0d0849ad7513ab",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
